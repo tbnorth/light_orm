90022616c8d4f64c539b1d6197685fdf604a3587
Terry Brown <terrynbrown@gmail.com>
2021-02-13 13:34:36 -0600

    more pip install rearrangement

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='light_orm',
+        version='0.1',
+        description='Package to allow github pip installation',
+        url='git@github.com:tbnorth/light_orm.git',
+        author='Terry N Brown',
+        author_email='terrynbrown@gmail.com',
+        license='unlicense',
+        packages=['light_orm'],
+        zip_safe=False
+)
